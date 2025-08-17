552ebc04c4d3a2e8ff38c77bdbfa2cbb95a37171
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2025-08-17 20:05:09 +0100

    created notification banner css

--- daily-prayer-time-for-mosques-extended.php
+++ daily-prayer-time-for-mosques-extended.php
@@ -28,6 +28,7 @@ if (!class_exists('DailyPrayerTimeExtended')) {
       require_once plugin_dir_path(__FILE__) . "shortcodes/timetable_date/shortcode.php";
       require_once plugin_dir_path(__FILE__) . "shortcodes/timetable/shortcode.php";
       require_once plugin_dir_path(__FILE__) . "shortcodes/clock/shortcode.php";
+      require_once plugin_dir_path(__FILE__) . "shortcodes/notification_banner/shortcode.php";
     }
   }
 
---
+++ shortcodes/notification_banner/shortcode.php
@@ -0,0 +1,18 @@
+<?php
+
+function dpte_notification_banner_shortcode() {
+  wp_enqueue_style("dpte-notification-banner", plugin_dir_url(__FILE__) . "styles.css");
+  wp_enqueue_script("dpte-notification-banner", plugin_dir_url(__FILE__) . "script.js", [], null, true);
+
+  ob_start();
+  ?>
+  <div class="notification-banner active">
+    <div class="notification-banner-content">
+      Asr Jama'ah in 30:20
+    </div>
+  </div>
+  <?php
+  return ob_get_clean();
+}
+
+add_shortcode('dpte_notification_banner', 'dpte_notification_banner_shortcode');
