9a5bf1d7eb54faf6334b060e0e7dc9ada6d4abad
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2025-08-22 05:19:58 +0100

    created admin page for notification banner

--- shortcodes/notification_banner/admin.php
+++ shortcodes/notification_banner/admin.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('ABSPATH')) {
-	exit;
+  exit;
 }
 
 use Carbon_Fields\Container;
@@ -9,13 +9,53 @@ use Carbon_Fields\Field;
 
 add_action('dpte_extend_notification_banner_container', function($container) {
   $container
-    ->add_tab(__('Notification Banner'), [
+    ->add_tab(__('[dpte_notification_banner]'), [
       Field::make('html', 'dpte_notification_banner_heading')
         ->set_html('
-          <h2 style="padding: 0; font-size: 1.25rem; font-weight: 500;">Notification Banner - [dpte_notification_banner]</h2>
-          <p>Quisque mattis ligula.</p>'
-        ),
-      Field::make('color', 'dpte_notification_banner_color', 'Notification Banner Highlight Color')
-        ->set_default_value('#ff0000'),
+          <h2 style="padding: 0; font-size: 1.25rem; font-weight: 500;">[dpte_notification_banner]</h2>
+          <p>Change settings for the <b>[dpte_notification_banner]</b> shortcode.</p>
+        '),
+      Field::make('html', 'crb_separator_1')
+        ->set_html('<h2 style="padding: 0; margin: 0; margin-top: 1rem; font-size: 1rem; font-weight: 500;">Colors</h2>'),
+      Field::make('color', 'dpte_notification_banner_active_background', 'Normal Notification Background Color')
+        ->set_default_value('#CFA55B')
+        ->set_help_text('Change the background color of normal notification banners.'),
+      Field::make('color', 'dpte_notification_banner_active_color', 'Normal Notification Text Color')
+        ->set_default_value('#2C2C2E')
+        ->set_help_text('Change the text color of normal notification banners.'),
+      Field::make('color', 'dpte_notification_banner_active_icon_color', 'Normal Notification Icon Color')
+        ->set_default_value('#2C2C2E')
+        ->set_help_text('Change the icon color of normal notification banners.'),
+      Field::make('color', 'dpte_notification_banner_error_background', 'Error Notification Background Color')
+        ->set_default_value('#ac0000')
+        ->set_help_text('Change the background color of error notification banners (such as Zawal time notifications).'),
+      Field::make('color', 'dpte_notification_banner_error_color', 'Error Notification Text Color')
+        ->set_default_value('#FFFFFF')
+        ->set_help_text('Change the text color of error notification banners (such as Zawal time notifications).'),
+      Field::make('color', 'dpte_notification_banner_error_icon_color', 'Error Notification Icon Color')
+        ->set_default_value('#FFFFFF')
+        ->set_help_text('Change the icon color of error notification banners (such as Zawal time notifications).'),
     ]);
+});
+
+add_action('wp_head', function() {
+  $dpte_notification_banner_active_background = carbon_get_theme_option('dpte_notification_banner_active_background');
+  $dpte_notification_banner_active_color = carbon_get_theme_option('dpte_notification_banner_active_color');
+  $dpte_notification_banner_active_icon_color = carbon_get_theme_option('dpte_notification_banner_active_icon_color');
+  $dpte_notification_banner_error_background = carbon_get_theme_option('dpte_notification_banner_error_background');
+  $dpte_notification_banner_error_color = carbon_get_theme_option('dpte_notification_banner_error_color');
+  $dpte_notification_banner_error_icon_color = carbon_get_theme_option('dpte_notification_banner_error_icon_color');
+
+  echo "
+    <style>
+      :root {
+        --dpte-notification-banner-active-background: {$dpte_notification_banner_active_background};
+        --dpte-notification-banner-active-color: {$dpte_notification_banner_active_color};
+        --dpte-notification-banner-active-icon-color: {$dpte_notification_banner_active_icon_color};
+        --dpte-notification-banner-error-background: {$dpte_notification_banner_error_background};
+        --dpte-notification-banner-error-color: {$dpte_notification_banner_error_color};
+        --dpte-notification-banner-error-icon-color: {$dpte_notification_banner_error_icon_color};
+      }
+    </style>
+  ";
 });
--- shortcodes/notification_banner/shortcode.php
+++ shortcodes/notification_banner/shortcode.php
@@ -12,11 +12,7 @@ function dpte_notification_banner_shortcode() {
   ?>
   <div class="dpte-notification-banner">
     <div class="dpte-notification-banner-content">
-      <svg class="dpte-notification-error-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-label="No entry" fill="none" stroke="#ffffff">
-        <circle cx="12" cy="12" r="10" stroke="inherit" stroke-width="3"/>
-        <line x1="5.6" y1="5.6" x2="18.4" y2="18.4" stroke="inherit" stroke-width="3"/>
-      </svg>
-      <svg class="dpte-notification-active-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200" fill="#2C2C2E">
+      <svg class="dpte-notification-active-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
         <path d="M60 110 Q100 40 140 110 Z" /> <!-- Main dome -->
         <rect x="50" y="110" width="100" height="50" /> <!-- Central body -->
         <rect x="90" y="130" width="20" height="30"/> <!-- Door -->
@@ -24,6 +20,10 @@ function dpte_notification_banner_shortcode() {
         <polygon points="25,70 35,50 45,70" /> <!-- Left minaret -->
         <rect x="160" y="70" width="10" height="90" /> <!-- Right minaret -->
         <polygon points="155,70 165,50 175,70" /> <!-- Right minaret -->
+      </svg>
+      <svg class="dpte-notification-error-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-label="No entry" fill="none">
+        <circle cx="12" cy="12" r="10" stroke="inherit" stroke-width="3"/>
+        <line x1="5.6" y1="5.6" x2="18.4" y2="18.4" stroke="inherit" stroke-width="3"/>
       </svg>
       <p class="dpte-notification-text">...</p>
     </div>
