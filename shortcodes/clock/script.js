f522445b9c71f85cf9f3d2e141850952dd7e0574
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2025-09-01 21:09:06 +0100

    namespaced clock

--- shortcodes/clock/script.js
+++ shortcodes/clock/script.js
@@ -1,70 +1,73 @@
-function setTimeRemainingMessage() {
-  const currentPrayer = dptCache.getCurrentPrayer();
-  const timeRemainingHeader = document.querySelectorAll(".dpte-clock .dpte-time-remaining-header");
-  const timeRemainingValue = document.querySelectorAll(".dpte-clock .dpte-time-remaining-value");
+class Clock {
+  static setTimeRemainingMessage() {
+    const currentPrayer = dptCache.getCurrentPrayer();
+    const timeRemainingHeader = document.querySelectorAll(".dpte-clock .dpte-time-remaining-header");
+    const timeRemainingValue = document.querySelectorAll(".dpte-clock .dpte-time-remaining-value");
 
-  if (!currentPrayer) {
-    timeRemainingHeader.forEach((element) => {
-      element.textContent = "";
+    if (!currentPrayer) {
+      timeRemainingHeader.forEach((element) => {
+        element.textContent = "";
+      });
+      timeRemainingValue.forEach((element) => {
+        element.textContent = "";
+      });
+    } else if (currentPrayer.waitingForJamah) {
+      timeRemainingHeader.forEach((element) => {
+        element.textContent = `${currentPrayer.name} Jama'ah in:`;
+      });
+      timeRemainingValue.forEach((element) => {
+        element.textContent = currentPrayer.timeRemaining;
+      });
+    } else {
+      timeRemainingHeader.forEach((element) => {
+        const nextPrayerName = dptCache.getNextPrayerName(currentPrayer.name);
+        const nextPrayerNameCapitalised = nextPrayerName.charAt(0).toUpperCase() + nextPrayerName.slice(1);
+        element.textContent = `Time to ${nextPrayerNameCapitalised}:`;
+      });
+      timeRemainingValue.forEach((element) => {
+        element.textContent = currentPrayer.timeRemaining;
+      });
+    }
+  }
+
+  // @source: clock adapted from https://www.youtube.com/watch?v=nVGhXcMROfU
+  static setClock() {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hoursRotation = (hours * 30) + (minutes * 6) / 12;
+    const minutesRotation = minutes * 6;
+    const secondsRotation = seconds * 6;
+
+    const hoursHand = document.querySelectorAll(".dpte-clock .dpte-hand-hours");
+    const minutesHand = document.querySelectorAll(".dpte-clock .dpte-hand-minutes");
+    const secondsHand = document.querySelectorAll(".dpte-clock .dpte-hand-seconds");
+
+    hoursHand.forEach((element) => {
+      element.style.transform = `rotate(${hoursRotation}deg)`;
     });
-    timeRemainingValue.forEach((element) => {
-      element.textContent = "";
+    minutesHand.forEach((element) => {
+      element.style.transform = `rotate(${minutesRotation}deg)`;
     });
-  } else if (currentPrayer.waitingForJamah) {
-    timeRemainingHeader.forEach((element) => {
-      element.textContent = `${currentPrayer.name} Jama'ah in:`;
-    });
-    timeRemainingValue.forEach((element) => {
-      element.textContent = currentPrayer.timeRemaining;
-    });
-  } else {
-    timeRemainingHeader.forEach((element) => {
-      const nextPrayerName = dptCache.getNextPrayerName(currentPrayer.name);
-      const nextPrayerNameCapitalised = nextPrayerName.charAt(0).toUpperCase() + nextPrayerName.slice(1);
-      element.textContent = `Time to ${nextPrayerNameCapitalised}:`;
-    });
-    timeRemainingValue.forEach((element) => {
-      element.textContent = currentPrayer.timeRemaining;
+    secondsHand.forEach((element) => {
+      element.style.transform = `rotate(${secondsRotation}deg)`;
     });
   }
 }
 
-// @source: clock adapted from https://www.youtube.com/watch?v=nVGhXcMROfU
-function setClock() {
-  const now = new Date();
-  const hours = now.getHours();
-  const minutes = now.getMinutes();
-  const seconds = now.getSeconds();
-
-  const hoursRotation = (hours * 30) + (minutes * 6) / 12;
-  const minutesRotation = minutes * 6;
-  const secondsRotation = seconds * 6;
-
-  const hoursHand = document.querySelectorAll(".dpte-clock .dpte-hand-hours");
-  const minutesHand = document.querySelectorAll(".dpte-clock .dpte-hand-minutes");
-  const secondsHand = document.querySelectorAll(".dpte-clock .dpte-hand-seconds");
-
-  hoursHand.forEach((element) => {
-    element.style.transform = `rotate(${hoursRotation}deg)`;
-  });
-  minutesHand.forEach((element) => {
-    element.style.transform = `rotate(${minutesRotation}deg)`;
-  });
-  secondsHand.forEach((element) => {
-    element.style.transform = `rotate(${secondsRotation}deg)`;
-  });
-}
 
 addEventListener("DOMContentLoaded", () => {
   dptCache.ensurePrayerData().then(() => {
-    setTimeRemainingMessage();
-    setTimeout(setTimeRemainingMessage, 1000);
-    setTimeout(setTimeRemainingMessage, 2500);
-    setInterval(setTimeRemainingMessage, 1000);
+    Clock.setTimeRemainingMessage();
+    setTimeout(Clock.setTimeRemainingMessage, 1000);
+    setTimeout(Clock.setTimeRemainingMessage, 2500);
+    setInterval(Clock.setTimeRemainingMessage, 1000);
 
-    setClock();
-    setTimeout(setClock, 1000);
-    setTimeout(setClock, 2500);
-    setInterval(setClock, 1000);
+    Clock.setClock();
+    setTimeout(Clock.setClock, 1000);
+    setTimeout(Clock.setClock, 2500);
+    setInterval(Clock.setClock, 1000);
   });
 });
