0b7d4479642657ece31bd854eaefc67566cf0be5
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2025-12-15 14:38:31 +0000

    minor updated to example shortcode

---
+++ shortcodes/example/script.js
@@ -0,0 +1,7 @@
+class Example {
+
+}
+
+addEventListener("DOMContentLoaded", () => {
+
+});
--- shortcodes/example/shortcode.php
+++ shortcodes/example/shortcode.php
@@ -5,8 +5,8 @@ if (!defined('ABSPATH')) {
 }
 
 function dpte_example_shortcode() {
-  wp_enqueue_style("dpte-example", plugin_dir_url(__FILE__) . "styles.css");
-  wp_enqueue_script("dpte-example", plugin_dir_url(__FILE__) . "script.js", [], null, true);
+  wp_enqueue_style("dpte_example", plugin_dir_url(__FILE__) . "styles.css");
+  wp_enqueue_script("dpte_example", plugin_dir_url(__FILE__) . "script.js", [], null, true);
 
   ob_start();
   ?>
