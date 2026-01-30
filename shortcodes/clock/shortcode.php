fc17a16e93ffb1257fc5935e22eac83f50bc6004
Murtaza Akhtar <murtaza-akhtar1@hotmail.com>
2026-01-30 20:15:58 +0000

    changed clock shortcode to take style attributes from shortcode properties instead of carbon fields

--- shortcodes/clock/shortcode.php
+++ shortcodes/clock/shortcode.php
@@ -9,17 +9,93 @@ function dpte_clock_shortcode($atts) {
   wp_enqueue_style("dpte_clock", plugin_dir_url(__FILE__) . "styles.css");
   wp_enqueue_script("dpte_clock", plugin_dir_url(__FILE__) . "script.js", ["dpte_dpt_cache"], null, true);
 
-  $data_attrs = '';
-  $atts = shortcode_atts(
-    array(
-      'showimages' => 'true',
-      'numberslanguage' => 'english'
-    ),
-    $atts,
-    'dpte_notification_banner'
-  );
+  // change all kebab-case attributes to snake_case attributes
+  $normalized_atts = array();
   foreach ($atts as $key => $value) {
-    $data_attrs .= ' data-' . esc_attr($key) . '="' . esc_attr($value) . '"';
+    $normalized_key = str_replace('-', '_', $key);
+    $normalized_atts[$normalized_key] = $value;
+  }
+  $atts = $normalized_atts;
+
+  // merge default atts and with user-provided atts
+  $default_atts = array(
+    // general
+    'show_images' => 'true',
+    'numbers_language' => 'english',
+    'clock_center_logo_image' => '',
+    'clock_center_background_logo_image' => '',
+    'clock_top_left_image' => '',
+    'clock_top_right_image' => '',
+    'clock_bottom_left_image' => '',
+    'clock_bottom_right_image' => '',
+
+    // css
+    'clock_background_gradient_1' => '#CFA55B',
+    'clock_background_gradient_2' => '#2C2C2E',
+    'clock_border' => '#2C2C2E',
+    'clock_number_color' => '#FFFFFF',
+    'clock_minute_line_color' => '#FFFFFF',
+    'clock_hand_hour_color_gradient_1' => '#ff5e9a',
+    'clock_hand_hour_color_gradient_2' => '#ffb960',
+    'clock_hand_minute_color_gradient_1' => '#ff5e9a',
+    'clock_hand_minute_color_gradient_2' => '#ffb960',
+    'clock_hand_second_color_gradient_1' => '#bbbbbbc7',
+    'clock_hand_second_color_gradient_2' => '#bbbbbbc7',
+    'clock_center_dot_background' => '#CFA55B',
+    'clock_center_dot_border' => '#2C2C2E',
+    'clock_time_remaining_color' => '#FFFFFF',
+    'clock_center_logo_image_scale' => '1',
+    'clock_center_logo_image_translate_x' => '0',
+    'clock_center_logo_image_translate_y' => '0',
+    'clock_center_background_logo_image_scale' => '1',
+    'clock_center_background_logo_image_translate_x' => '0',
+    'clock_center_background_logo_image_translate_y' => '0',
+    'clock_top_left_image_scale' => '1',
+    'clock_top_left_image_translate_x' => '0',
+    'clock_top_left_image_translate_y' => '0',
+    'clock_top_right_image_scale' => '1',
+    'clock_top_right_image_translate_x' => '0',
+    'clock_top_right_image_translate_y' => '0',
+    'clock_bottom_left_image_scale' => '1',
+    'clock_bottom_left_image_translate_x' => '0',
+    'clock_bottom_left_image_translate_y' => '0',
+    'clock_bottom_right_image_scale' => '1',
+    'clock_bottom_right_image_translate_x' => '0',
+    'clock_bottom_right_image_translate_y' => '0',
+
+    // js
+    // ...
+  );
+  $atts = shortcode_atts($default_atts, $atts, 'dpte_clock');
+
+  // generate css properties from atts
+  $style_keys = array(
+    'clock_background_gradient_1', 'clock_background_gradient_2', 'clock_border', 'clock_number_color',
+    'clock_minute_line_color', 'clock_hand_hour_color_gradient_1', 'clock_hand_hour_color_gradient_2',
+    'clock_hand_minute_color_gradient_1', 'clock_hand_minute_color_gradient_2',
+    'clock_hand_second_color_gradient_1', 'clock_hand_second_color_gradient_2',
+    'clock_center_dot_background', 'clock_center_dot_border', 'clock_time_remaining_color',
+    'clock_center_logo_image_scale', 'clock_center_logo_image_translate_x', 'clock_center_logo_image_translate_y',
+    'clock_center_background_logo_image_scale', 'clock_center_background_logo_image_translate_x', 'clock_center_background_logo_image_translate_y',
+    'clock_top_left_image_scale', 'clock_top_left_image_translate_x', 'clock_top_left_image_translate_y',
+    'clock_top_right_image_scale', 'clock_top_right_image_translate_x', 'clock_top_right_image_translate_y',
+    'clock_bottom_left_image_scale', 'clock_bottom_left_image_translate_x', 'clock_bottom_left_image_translate_y',
+    'clock_bottom_right_image_scale', 'clock_bottom_right_image_translate_x', 'clock_bottom_right_image_translate_y',
+  );
+  $style = '';
+  foreach ($style_keys as $key) {
+    if (isset($atts[$key])) {
+      $style .= '--dpte-' . esc_attr(str_replace('_', '-', $key)) . ':' . esc_attr($atts[$key]) . ';';
+    }
+  }
+
+  // generate data attributes from atts
+  $data_keys  = array();
+  $data_attrs = '';
+  foreach ($data_keys as $key) {
+    if (isset($atts[$key])) {
+      $data_attrs .= ' data-' . esc_attr(str_replace('_', '-', $key)) . '="' . esc_attr($atts[$key]) . '"';
+    }
   }
 
   $numbers = [
@@ -34,12 +110,12 @@ function dpte_clock_shortcode($atts) {
       9 => '٩', 10 => '١٠', 11 => '١١', 12 => '١٢',
     ],
   ];
-  $lang = $atts['numberslanguage'] ?? 'english';
+  $lang = $atts['numbers_language'] ?? 'english';
 
   ob_start();
   ?>
-  <div class="dpte-clock-wrapper" <?php echo $data_attrs; ?>>
-    <img class="dpte-center-logo-background" src="<?php echo esc_url(carbon_get_theme_option('dpte_clock_center_logo_image_background')); ?>" alt="">
+  <div class="dpte-clock-wrapper" style="<?php echo esc_attr($style); ?>" <?php echo $data_attrs; ?>>
+    <img class="dpte-center-background-logo" src="<?php echo esc_url($atts['clock_center_background_logo_image']); ?>" aria-hidden="true">
     <div class="dpte-clock">
       <ul class="dpte-hours-container">
         <?php
@@ -65,7 +141,7 @@ function dpte_clock_shortcode($atts) {
         <div class="dpte-hand dpte-hand-seconds"></div>
       </div>
       <div class="dpte-center-dot"></div>
-      <img class="dpte-center-logo" src="<?php echo esc_url(carbon_get_theme_option('dpte_clock_center_logo_image')); ?>" alt="">
+      <img class="dpte-center-logo" src="<?php echo esc_url($atts['clock_center_logo_image']); ?>" aria-hidden="true">
       <div class="dpte-time-remaining">
         <div class="dpte-time-remaining-content">
           <span class="dpte-time-remaining-header"></span>
@@ -73,36 +149,36 @@ function dpte_clock_shortcode($atts) {
         </div>
       </div>
       <img
-        <?php if (isset($atts['showimages']) && $atts['showimages'] === "false") : ?>
+        <?php if (isset($atts['show_images']) && $atts['show_images'] === "false") : ?>
           style="display: none;"
         <?php endif; ?>
         class="dpte-top-left-image"
-        src="<?php echo esc_url(carbon_get_theme_option('dpte_clock_top_left_image')); ?>"
-        alt=""
+        src="<?php echo esc_url($atts['clock_top_left_image']); ?>"
+        aria-hidden="true"
       >
       <img
-        <?php if (isset($atts['showimages']) && $atts['showimages'] === "false") : ?>
+        <?php if (isset($atts['show_images']) && $atts['show_images'] === "false") : ?>
           style="display: none;"
         <?php endif; ?>
         class="dpte-top-right-image"
-        src="<?php echo esc_url(carbon_get_theme_option('dpte_clock_top_right_image')); ?>"
-        alt=""
+        src="<?php echo esc_url($atts['clock_top_right_image']); ?>"
+        aria-hidden="true"
       >
       <img
-        <?php if (isset($atts['showimages']) && $atts['showimages'] === "false") : ?>
+        <?php if (isset($atts['show_images']) && $atts['show_images'] === "false") : ?>
           style="display: none;"
         <?php endif; ?>
         class="dpte-bottom-left-image"
-        src="<?php echo esc_url(carbon_get_theme_option('dpte_clock_bottom_left_image')); ?>"
-        alt=""
+        src="<?php echo esc_url($atts['clock_bottom_left_image']); ?>"
+        aria-hidden="true"
       >
       <img
-        <?php if (isset($atts['showimages']) && $atts['showimages'] === "false") : ?>
+        <?php if (isset($atts['show_images']) && $atts['show_images'] === "false") : ?>
           style="display: none;"
         <?php endif; ?>
         class="dpte-bottom-right-image"
-        src="<?php echo esc_url(carbon_get_theme_option('dpte_clock_bottom_right_image')); ?>"
-        alt=""
+        src="<?php echo esc_url($atts['clock_bottom_right_image']); ?>"
+        aria-hidden="true"
       >
     </div>
   </div>
